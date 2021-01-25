faf39bc17b8c96563a9eced90e3c46073d7d9f64
Freewind <nowindlee@gmail.com>
2021-01-25 15:45:56 +0800

    typescript-react-render-component-to-html-demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-TypeScript React Render Component to HTML Demo
+TypeScript React Render Component to Static HTML Demo
 =================================
 
 使用`ReactDOMServer`中的方法可以把一个React组件转换成Html/String
+
+但注意的是，它只能转换静态内容，不能转换state
 
 ```
 npm install
--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,12 +1,13 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
 type Props = {
   defaultValue: string
 }
 
 export const MyComponent: FC<Props> = ({defaultValue}) => {
+  const [value, setValue] = useState(defaultValue)
   return <div style={{color: 'red'}}>
     <span>Hello</span>
-    <input type={'text'} defaultValue={defaultValue}/>
+    <input type={'text'} value={value} onChange={event => setValue(event.target.value)}/>
   </div>;
 }
